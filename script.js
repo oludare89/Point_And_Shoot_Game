22d252b723f25d2a12c1e634660c53cebba0bcd6
oludare89 <azizoyekan@hotmail.com>
2022-11-10 17:24:37 +0000

    When ravens hit top or bottom of screen edge then will reverse vertical direction

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ class Raven {
         this.flapInternval = Math.random * 50 + 50;
     }
     update(deltatime){
+        if (this.y < 0 || this.y > canvas.height - this.width){
+            this.directionY = this.directionY * -1;
+        }
         this.x -= this.directionX;
         this.y += this.directionY;
         if (this.x < 0 - this.width) this.markedForDeletion = true;
