2f40f2e756d73a822f3eeeb26711dd04f42e6230
oludare89 <azizoyekan@hotmail.com>
2022-11-11 15:13:37 +0000

    Added particle class for trails behind ravens and amended background gradient - particle issues to resolve

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ class Raven {
         this.flapInternval = Math.random * 50 + 50;
         this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
         this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')';
+        this.hasTrail = Math.random() > 0.5;
     }
     update(deltatime){
         if (this.y < 0 || this.y > canvas.height - this.width){
@@ -49,6 +50,11 @@ class Raven {
             if (this.frame > this.maxFrame) this.frame = 0;
             else this.frame++;
             this.timeSinceFlap = 0;
+            if (this.hasTrail){
+                for (let i = 0; i < 5; i++){
+                    particles.push(new Particle(this.x, this.y, this.width, this.color));
+                }
+            }     
         }
         if (this.x < 0 - this.width) gameOver = true;        
     }
@@ -90,6 +96,34 @@ class Explosion {
     }
 }
 
+let particles = [];
+class Particle {
+    constructor(x, y, size, color){
+        this.size = size;
+        this.x = x + this.size/2 + Math.random() * 50 - 25;
+        this.y = y + this.size/3 + Math.random() * 50 - 25;
+        this.radius = Math.random() * this.size/10;
+        this.maxRadius = Math.random() * 20 + 35;
+        this.markedForDeletion = false;
+        this.speedX = Math.random () * 1 + 0.5;
+        this.color = color;
+    }
+    update(){
+        this.x += this.speedX;
+        this.radius += 0.5;
+        if (this.radius > this.maxRadius - 5) this.markedForDeletion = true;
+    }
+    draw(){
+        ctx.save();
+        ctx.globalAlpha = 1 - this.radius/this.maxRadius;
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.restore();
+    }
+}
+
 function drawScore(){
     ctx.fillStyle = 'black';
     ctx.fillText('Score: ' + score, 50, 75);
@@ -100,14 +134,13 @@ function drawScore(){
 function drawGameOver(){
     ctx.textAlign = 'center';
     ctx.fillStyle = 'black';
-    ctx.fillText('GAME OVER! Your score is ' + score, canvas.width/2, canvas.height/2);
+    ctx.fillText('GAME OVER! Your score is ' + score + '. Please reload page to play again.', canvas.width/2, canvas.height/2);
     ctx.fillStyle = 'white';
-    ctx.fillText('GAME OVER! Your score is ' + score, canvas.width/2 + 5, canvas.height/2 + 5);
+    ctx.fillText('GAME OVER! Your score is ' + score + '. Please reload page to play again.', canvas.width/2 + 5, canvas.height/2 + 5);
 }
 
 window.addEventListener('click', function(e){
     const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1);
-    console.log(detectPixelColor);
     const pc = detectPixelColor.data;
     ravens.forEach(object => {
         if (object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1] && object.randomColors[2] === pc[2] ) {
@@ -133,10 +166,11 @@ function animate(timestamp){
         })
     };
     drawScore();
-    [...ravens, ...explosions].forEach(object => object.update(deltatime));
-    [...ravens, ...explosions].forEach(object => object.draw());
+    [...particles, ...ravens, ...explosions].forEach(object => object.update(deltatime));
+    [...particles, ...ravens, ...explosions, ...particles].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion);
     explosions = explosions.filter(object => !object.markedForDeletion);
+    particles = particles.filter(object => !object.markedForDeletion);
     if (!gameOver) requestAnimationFrame(animate);
     else drawGameOver();
 }
